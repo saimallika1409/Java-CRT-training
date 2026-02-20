7719dbc265e324461dd92d1bbf0d169d7c4f39b9
saimallika <saichetkuri289@gmail.com>
2026-02-20 11:03:30 +0530

    Added 3 new Java programs

---
+++ Loops.java
@@ -0,0 +1,12 @@
+public class Loops {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=100) {
+            System.out.println(i);
+            i++;
+        }
+        for(int j=10;j<=1;j--) {
+            System.out.println(j);
+        }
+    }
+}
