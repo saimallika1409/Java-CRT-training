23de44ee6ca1c1214668b6f94ed6b8f8cc2363d2
saimallika <saichetkuri289@gmail.com>
2026-02-20 11:25:26 +0530

    Added 2 new Java programs

---
+++ CountDigits.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int count = 0;
+
+        if (num == 0) {
+            count = 1;
+        } else {
+            while (num != 0) {
+                count++;
+                num = num / 10;
+            }
+        }
+
+        System.out.println("Number of digits = " + count);
+    }
+}
---
+++ ReverseNumbers.java
@@ -0,0 +1,11 @@
+public class ReverseNumbers {
+    public static void main(String[] args) {
+
+        int i = 50;
+
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
